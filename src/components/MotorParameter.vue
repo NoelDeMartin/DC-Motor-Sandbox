061fe892b3923c5571beaeb729c5fd74e9eaa533
Noel De Martin <noeldemartin@gmail.com>
2018-03-08 18:53:05 +0100

    Fix parameter locks

--- src/components/MotorParameter.vue
+++ src/components/MotorParameter.vue
@@ -3,39 +3,32 @@
 
         <span class="w-32 mr-2">{{ name }}</span>
 
-        <div class="relative mr-2">
-            <svg
-                class="absolute pin"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-            >
-                <path
-                    v-if="locked"
-                    class="opacity-25"
-                    d="M4
-                        8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2
-                        2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5
-                        6.73V17h2v-2.27a2 2 0 1 0-2 0zM7
-                        6v2h6V6a3 3 0 0 0-6 0z
-                    "
-                />
-                <path
-                    v-else
-                    d="M4
-                        8V6a6 6 0 1 1 12 0h-3v2h4a2 2 0 0 1 2 2v8a2 2 0 0 1-2
-                        2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5
-                        6.73V17h2v-2.27a2 2 0 1 0-2 0zM7
-                        6v2h6V6a3 3 0 0 0-6 0z
-                    "
-                />
-            </svg>
-            <input
-                :checked="locked"
-                class="relative opacity-0 cursor-pointer"
-                type="checkbox"
-                @input="e => $emit(e.target.checked ? 'lock' : 'unlock')"
-            >
-        </div>
+        <svg
+            class="w-4 h-4 cursor-pointer mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            @click="$emit(locked ? 'unlock' : 'lock')"
+        >
+            <path
+                v-if="locked"
+                class="opacity-25"
+                d="M4
+                    8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2
+                    2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5
+                    6.73V17h2v-2.27a2 2 0 1 0-2 0zM7
+                    6v2h6V6a3 3 0 0 0-6 0z
+                "
+            />
+            <path
+                v-else
+                d="M4
+                    8V6a6 6 0 1 1 12 0h-3v2h4a2 2 0 0 1 2 2v8a2 2 0 0 1-2
+                    2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5
+                    6.73V17h2v-2.27a2 2 0 1 0-2 0zM7
+                    6v2h6V6a3 3 0 0 0-6 0z
+                "
+            />
+        </svg>
 
         <input
             :class="{'opacity-25': locked}"
